36230c7f3a27156fa8ab94482a0b2702a3523f9c
PC-BLANC <PC-BLANC@192.0.0.126>
2019-01-21 15:09:38 +0100

    r

--- Liste_Associations_Commune_Carte_CCCD/class/mypdo.class.php
+++ Liste_Associations_Commune_Carte_CCCD/class/mypdo.class.php
@@ -1,34 +1,34 @@
 <?php
 class mypdo extends PDO{
-
+    
     private $PARAM_hote='localhost'; // le chemin vers le serveur
     private $PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
     private $PARAM_mot_passe=''; // mot de passe de l'utilisateur pour se connecter
-    private $PARAM_nom_bd='association_cccd'; // nom de la base de donnée ustilisée
+    private $PARAM_nom_bd='association_cccd'; // nom de la base de donn�e ustilis�e
     private $connexion;
     public function __construct() {
-    	try {
-    		
-    		$this->connexion = new PDO('mysql:host='.$this->PARAM_hote.';dbname='.$this->PARAM_nom_bd, $this->PARAM_utilisateur, $this->PARAM_mot_passe,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-    		//echo '<script>alert ("ok connex");</script>)';echo $this->PARAM_nom_bd;
-    	}
-    	catch (PDOException $e)
-    	{
-    		echo 'hote: '.$this->PARAM_hote.' '.$_SERVER['DOCUMENT_ROOT'].'<br />';
-    		echo 'Erreur : '.$e->getMessage().'<br />';
-    		echo 'NÂ° : '.$e->getCode();
-    		$this->connexion=false;
-    		//echo '<script>alert ("pbs acces bdd");</script>)';
-    	}
+        try {
+            
+            $this->connexion = new PDO('mysql:host='.$this->PARAM_hote.';dbname='.$this->PARAM_nom_bd, $this->PARAM_utilisateur, $this->PARAM_mot_passe,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            //echo '<script>alert ("ok connex");</script>)';echo $this->PARAM_nom_bd;
+        }
+        catch (PDOException $e)
+        {
+            echo 'hote: '.$this->PARAM_hote.' '.$_SERVER['DOCUMENT_ROOT'].'<br />';
+            echo 'Erreur : '.$e->getMessage().'<br />';
+            echo 'N° : '.$e->getCode();
+            $this->connexion=false;
+            //echo '<script>alert ("pbs acces bdd");</script>)';
+        }
     }
     public function __get($propriete) {
-    	switch ($propriete) {
-    		case 'connexion' :
-    			{
-    				return $this->connexion;
-    				break;
-    			}
-    	}
+        switch ($propriete) {
+            case 'connexion' :
+                {
+                    return $this->connexion;
+                    break;
+                }
+        }
     }
     
     public function liste_associations($nomCommune)
@@ -40,7 +40,7 @@ class mypdo extends PDO{
             and a.idCommune=cm.idCommune
             and a.idPresident=p.idPresident
             and cm.nomComm ="'.$nomCommune.'"
-            order by nomCategorie';       
+            order by nomCategorie';
         $result=$this->connexion ->query($requete);
         if ($result)
         {
