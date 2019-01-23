6be9f5bc72b36253aacb72aa5c7bf370f503d114
PC-BLANC <PC-BLANC@192.0.0.126>
2019-01-23 14:11:25 +0100

    connexion delete

--- Liste_Associations_Commune_Carte_CCCD/index.php
+++ Liste_Associations_Commune_Carte_CCCD/index.php
@@ -46,26 +46,6 @@ switch ($params[2]) {
             $site->affiche();  
         }
         break;
-
-    case 'connexion':
-        $site->titre = 'Connexion';
-        $site->js = 'jquery.validate.min';
-        $site->js = 'messages_fr';
-        $site->js = 'jquery.tooltipster.min';
-        $site->css = 'tooltipster';
-        $site->left_sidebar = $controleur->retourne_formulaire_login();
-        $site->affiche();
-        break;
-    case 'deconnexion':
-        $_SESSION = array();
-        session_destroy();
-        echo '<script>document.location.href="index.php"; </script>';
-        break;
-    default:
-        $site->titre = 'Accueil';
-        $site->left_sidebar = '<img src="' . $site->path . '/image/erreur-404.png" alt="Erreur de liens">';
-        $site->affiche();
-        break;
 }
 
 ?>
